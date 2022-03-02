dec0baf9a9c045f70f9f928557a8e02e7e6fabff
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2022-03-02 11:33:03 +0100

    🚧initialization

---
+++ TalkingApp.Shared/Extension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalkingApp.Shared
+{
+    /// <summary>
+    /// custom shared resgistering class.
+    /// </summary>
+    public static class Extension
+    {
+    }
+}
